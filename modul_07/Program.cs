fd7c6caa1624ea37a3f7b3fd7ea3005ba8914c68
rosell <Gustavrosell@gmail.com>
2023-10-31 09:55:33 +0100

    siu?

---
+++ modul_07/Program.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_07
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new BsonDocument
+            {
+                {"Name", "Gustav"},
+                {"Email", "Gustavrosell@gmail.com"},
+                {"Tlfnr", "31402442"}
+            };
+
+            Persistency persistency = new Persistency();
+        }
+    }
+}
---
+++ modul_07/storage/Persistency.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace modul_07
+{
+    public class Persistency
+    {
+        // KIG HER: hvis du igang med at stjæle min kode fra GitHub:
+        // Her indsætter du egen string samt password fra MongoDB // whatever SQL-program du bruger
+        // I MongoDB: Deployment --> Database --> find din database --> Connect --> MongoDB for VS Code --> kopier streng og indsæt password
+        private string connectionString = "mongodb+srv://gustavrosell:Gustav41541@dbdesign.q2bkeja.mongodb.net/";
+
+        // HUSK: Using MongoDB.Driver; Using MongoDB.Bson;
+        IMongoClient dbClient;
+        IMongoDatabase database;
+        IMongoCollection<BsonDocument> collection;
+
+
+        // Constructor? Tror jeg?
+        public Persistency()
+        {
+            dbClient = new MongoClient();
+            database = dbClient.GetDatabase("ansattedb");
+            collection = database.GetCollection<BsonDocument>("projects");
+        }
+
+        public void opretansatte(BsonDocument ansatte)
+        {
+            collection.InsertOne(ansatte);
+        }
+    }
+}
